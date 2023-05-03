9315d36e9219f44f7b10e4ea994e9739bc918160
Olivier <olivier.winkler@icloud.com>
2023-05-03 19:00:27 +0200

    feat(dialog): update form styling

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -2,6 +2,7 @@ import * as DialogRadix from '@radix-ui/react-dialog';
 import { useEffect } from 'react';
 import styles from './dialog.module.scss';
 import Button from '../button/button';
+import Image from 'next/image';
 
 type Props = {
   open: boolean;
@@ -24,11 +25,17 @@ export default function Dialog({ open, onClose }: Props) {
       <DialogRadix.Portal>
         <DialogRadix.Overlay className={styles.overlay} onClick={onClose} />
         <DialogRadix.Content className={styles.content}>
-          {/* Logo */}
-          <h3>Join Waitlist</h3>
-          <span>Join our growing waitlist and we will reach out to you as soon as possible.</span>
-          <label>Email address</label>
-          <input type="email" placeholder="Enter your mail" />
+          <Image src="/icons/logo.svg" alt="Daoify Logo" height={50} width={50} />
+
+          <div className={styles.header}>
+            <h3>Join Waitlist</h3>
+            <span>Join our growing waitlist and we will reach out to you as soon as possible.</span>
+          </div>
+
+          <div className={styles.form}>
+            <label>Email address</label>
+            <input type="email" placeholder="Enter your mail" />
+          </div>
 
           <Button>Join</Button>
         </DialogRadix.Content>
