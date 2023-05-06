ec1e2a921f9ede855602bbc8109fb8bb2ae7d474
Olivier <olivier.winkler@icloud.com>
2023-05-06 11:54:06 +0200

    feat(cobe): seperate cobe component

---
+++ src/components/cobe/cobe.tsx
@@ -0,0 +1,45 @@
+import createGlobe from "cobe";
+import { useRef, useEffect } from "react";
+
+export default function Cobe() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    let phi = 0;
+    let globe: any;
+
+    if (canvasRef?.current) {
+      globe = createGlobe(canvasRef.current, {
+        devicePixelRatio: 2,
+        width: 600 * 2,
+        height: 600 * 2,
+        phi: 0,
+        theta: 0,
+        dark: 1,
+        diffuse: 1.2,
+        mapSamples: 16000,
+        mapBrightness: 6,
+        baseColor: [0.3, 0.3, 0.3],
+        markerColor: [0.1, 0.8, 1],
+        glowColor: [1, 1, 1],
+        markers: [],
+        onRender: state => {
+          state.phi = phi;
+          phi += 0.01;
+        },
+      });
+    }
+
+    return () => {
+      globe.destroy();
+    };
+  }, []);
+
+
+  return (
+    <canvas
+      ref={canvasRef}
+      style={{ width: 600, height: 600, maxWidth: '100%', aspectRatio: 1 }}
+    />
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,13 @@
-import styles from '@/styles/Home.module.scss';
-import { Inter } from 'next/font/google';
-import Head from 'next/head';
-import createGlobe from 'cobe';
-import { useEffect, useRef, useState } from 'react';
 import Button from '@/components/button/button';
-import Image from 'next/image';
+import Cobe from '@/components/cobe/cobe';
 import Dialog from '@/components/dialog/dialog';
+import styles from '@/styles/Home.module.scss';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [open, setOpen] = useState(false);
-
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-
-  useEffect(() => {
-    let phi = 0;
-    let globe: any;
-
-    if (canvasRef?.current) {
-      globe = createGlobe(canvasRef.current, {
-        devicePixelRatio: 2,
-        width: 600 * 2,
-        height: 600 * 2,
-        phi: 0,
-        theta: 0,
-        dark: 1,
-        diffuse: 1.2,
-        mapSamples: 16000,
-        mapBrightness: 6,
-        baseColor: [0.3, 0.3, 0.3],
-        markerColor: [0.1, 0.8, 1],
-        glowColor: [1, 1, 1],
-        markers: [
-          // longitude latitude
-          // { location: [37.7595, -122.4367], size: 0.03 },
-          // { location: [40.7128, -74.006], size: 0.1 },
-        ],
-        onRender: state => {
-          // Called on every animation frame.
-          // `state` will be an empty object, return updated params.
-          state.phi = phi;
-          phi += 0.01;
-        },
-      });
-    }
-
-    return () => {
-      globe.destroy();
-    };
-  }, []);
 
   useEffect(() => {
     const onKeyPress = (e: KeyboardEvent) => {
@@ -77,10 +37,7 @@ export default function Home() {
       </nav>
 
       <main className={styles.main}>
-        <canvas
-          ref={canvasRef}
-          style={{ width: 600, height: 600, maxWidth: '100%', aspectRatio: 1 }}
-        />
+        <Cobe />
       </main>
 
       <Dialog
