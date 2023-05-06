42204099833c322860ffd441543f4351328706bb
Olivier <olivier.winkler@icloud.com>
2023-05-06 12:08:38 +0200

    feat(cobe): cobe autorotate + draggable

--- src/components/cobe/cobe.tsx
+++ src/components/cobe/cobe.tsx
@@ -1,9 +1,22 @@
 import { WidthIcon } from "@radix-ui/react-icons";
+import { useSpring } from "@react-spring/web";
 import createGlobe from "cobe";
 import { useRef, useEffect } from "react";
 
 export default function Cobe() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const pointerInteracting = useRef<number | null>(null);
+  const pointerInteractionMovement = useRef(0);
+
+  const [{ r }, api] = useSpring(() => ({
+    r: 0,
+    config: {
+      mass: 1,
+      tension: 280,
+      friction: 40,
+      precision: 0.001,
+    },
+  }));
 
   useEffect(() => {
     let phi = 0;
@@ -30,8 +43,10 @@ export default function Cobe() {
         glowColor: [1, 1, 1],
         markers: [],
         onRender: state => {
-          state.phi = phi;
-          phi += 0.01;
+          if (!pointerInteracting.current) {
+            phi += 0.005
+          }
+          state.phi = phi + r.get()
           state.width = width * 2
           state.height = width * 2
         },
@@ -50,7 +65,47 @@ export default function Cobe() {
   return (
     <canvas
       ref={canvasRef}
-      style={{ width: 600, height: 600, maxWidth: '100%', aspectRatio: 1 }}
+      onPointerDown={(e) => {
+        pointerInteracting.current =
+          e.clientX - pointerInteractionMovement.current;
+        canvasRef.current!.style.cursor = 'grabbing';
+      }}
+      onPointerUp={() => {
+        pointerInteracting.current = null;
+        canvasRef.current!.style.cursor = 'grab';
+      }}
+      onPointerOut={() => {
+        pointerInteracting.current = null;
+        canvasRef.current!.style.cursor = 'grab';
+      }}
+      onMouseMove={(e) => {
+        if (pointerInteracting.current !== null) {
+          const delta = e.clientX - pointerInteracting.current;
+          pointerInteractionMovement.current = delta;
+          api.start({
+            r: delta / 200,
+          });
+        }
+      }}
+      onTouchMove={(e) => {
+        if (pointerInteracting.current !== null && e.touches[0]) {
+          const delta = e.touches[0].clientX - pointerInteracting.current;
+          pointerInteractionMovement.current = delta;
+          api.start({
+            r: delta / 100,
+          });
+        }
+      }}
+      style={{
+        width: 600,
+        height: 600,
+        maxWidth: '100%',
+        aspectRatio: 1,
+        cursor: 'grab',
+        contain: 'layout paint size',
+        opacity: 0,
+        transition: 'opacity 1s ease',
+      }}
     />
   );
 };
