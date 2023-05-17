7d1ccb976300cc7b258ce4b252eea9fb164a6077
Olivier <olivier.winkler@icloud.com>
2023-05-17 23:02:31 +0200

    feat(app): basic tailwind app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,64 +1,21 @@
-import Button from '@/components/button/button';
-import Cobe from '@/components/cobe/cobe';
-import Dialog from '@/components/dialog/dialog';
-import styles from '@/styles/Home.module.scss';
-import Head from 'next/head';
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import Head from 'next/head'
+
 
 export default function Home() {
-  const [open, setOpen] = useState(false);
-
-  useEffect(() => {
-    const onKeyPress = (e: KeyboardEvent) => {
-      if (e.metaKey && e.key === 'Enter') {
-        setOpen(true);
-      }
-    };
-
-    window.addEventListener('keydown', onKeyPress);
-
-    return () => window.removeEventListener('keydown', onKeyPress);
-  }, []);
-
   return (
     <>
       <Head>
-        <title>DAOify - Next Gen Legal DAO Setup</title>
-        <meta name="description" content="DAOify" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Pocket - Invest at the perfect time.</title>
+        <meta
+          name="description"
+          content="By leveraging insights from our network of industry insiders, you’ll know exactly when to buy to maximize profit, and exactly when to sell to avoid painful losses."
+        />
       </Head>
-
-      <nav className={styles.nav}>
-        <Image src="/icons/logo.svg" alt="Daoify Logo" height={50} width={50} />
-
-        <Button onClick={() => setOpen(true)}>Join Waitlist</Button>
-      </nav>
-
-      <main className={styles.main}>
-        <div>
-          <h1>Latest Legal Wrapper for DAOs</h1>
-          <p>Lorem Impsum Lorem Impsum Lorem Impsum Lorem Impsum</p>
-
-          <div className={styles.cta} onClick={() => setOpen(true)}>
-            Press
-            <div className={styles.wrapper}>
-              <span className={styles.key}>⌘</span>
-              +
-              <span className={styles.key}>⏎</span>
-            </div>
-            for the waitlist
-          </div>
-        </div>
-        <Cobe />
+      <main>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </main>
-
-      <Dialog
-        open={open}
-        onClose={() => setOpen(false)}
-        onSubmit={console.log}
-      />
     </>
-  );
+  )
 }
