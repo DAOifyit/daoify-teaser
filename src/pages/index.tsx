5da23a434c4bb51a0042913548da8c8bc351aea9
Olivier <olivier.winkler@icloud.com>
2023-05-18 00:08:39 +0200

    feat(index): migrate from scss to tailwind styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import Button from '@/components/button/button';
 import Cobe from '@/components/cobe/cobe';
 import Dialog from '@/components/dialog/dialog';
-// import styles from '@/styles/Home.module.scss';
 import Head from 'next/head';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-const styles = {};
+
 export default function Home() {
   const [open, setOpen] = useState(false);
 
@@ -30,23 +29,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <nav className={styles.nav}>
+      <nav className="flex justify-between items-center w-full p-8">
         <Image src="/icons/logo.svg" alt="Daoify Logo" height={50} width={50} />
 
         <Button onClick={() => setOpen(true)}>Join Waitlist</Button>
       </nav>
 
-      <main className={styles.main}>
+      <main className="flex flex-col px-10 py-5 md:grid grid-cols-2 items-center px-26 py-8">
         <div>
-          <h1>Latest Legal Wrapper for DAOs</h1>
-          <p>Lorem Impsum Lorem Impsum Lorem Impsum Lorem Impsum</p>
+          <h1 className="text-5xl">Latest Legal Wrapper for DAOs</h1>
+          <p className='mt-4 mb-8 text-2xl'>Lorem Impsum Lorem Impsum Lorem Impsum Lorem Impsum</p>
 
-          <div className={styles.cta} onClick={() => setOpen(true)}>
+          <div className={"relative flex gap-2 cursor-pointer hover:opacity-50"} onClick={() => setOpen(true)}>
             Press
-            <div className={styles.wrapper}>
-              <span className={styles.key}>⌘</span>
+            <div>
+              <span className={"inline-flex h-5 w-5 justify-center items-center rounded bg-white text-black mr-1"}>⌘</span>
               +
-              <span className={styles.key}>⏎</span>
+              <span className={"inline-flex h-5 w-5 justify-center items-center rounded bg-white text-black ml-1"}>⏎</span>
             </div>
             for the waitlist
           </div>
